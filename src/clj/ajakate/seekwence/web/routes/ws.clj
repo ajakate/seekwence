8e5da17c305d7c102d50412c981ce4ddd3ef0856
Ajay Jakate <ajay.jakate@gmail.com>
2023-10-29 16:51:15 -0500

    broadcast message

--- src/clj/ajakate/seekwence/web/routes/ws.clj
+++ src/clj/ajakate/seekwence/web/routes/ws.clj
@@ -28,10 +28,13 @@
      (send-fn uid [id response]))))
 
 (defmethod on-message :seekwence/game-info
-  [{:keys [id client-id ?data send-fn] :as message}]
+  [{:keys [id client-id ?data send-fn connected-uids] :as message}]
   (let [node (db-node message)
         resp (game/get-common-info-by-client-id node client-id)]
-    (send-fn client-id [id resp])))
+    (send-fn client-id [id resp])
+    (doseq [uid (:any @connected-uids)]
+      (when (= (game/game-code-for-player-id node uid) (:game/id resp))
+        (send-fn uid [id resp])))))
 
 (defmethod ig/init-key :sente/connection
   [_ opts]
